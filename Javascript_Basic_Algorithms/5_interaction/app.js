3f166efe2f5c970e16fade10be9eea0bb16691d5
Sergio0291 <sergio@ghfer.com>
2022-09-30 20:44:40 +0200

    Trabajando la 5º interacción con alas funciones

---
+++ Javascript_Basic_Algorithms/5_interaction/app.js
@@ -0,0 +1,30 @@
+const number1 = 10;
+const number2 = 20;
+const number3 = 2;
+
+if(number1 === 10){
+    console.log("True");
+}
+
+if (number2/number1 === 2) {
+  console.log("True");
+}
+if (number1 < or > number2) {
+  console.log("True");
+}
+
+if (number3 < o > number1) {
+  console.log("True");
+}
+
+if ((number3*5) = number1) {
+  console.log("True");
+}
+
+if ((number3*5) = number1 (number1*2) = number2) { 
+  console.log("True");
+}
+
+if (number(2/2) = number1 (number1/5) = number3) {
+  console.log("True");
+}
